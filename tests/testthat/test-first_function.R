c60a674b4c144395596e9798c3a93183646c500e
gunthergl <gunthergl@gmx.net>
2023-09-26 15:06:56 +0200

    testing first functions

--- tests/testthat/test-first_function.R
+++ tests/testthat/test-first_function.R
@@ -1,3 +1,7 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+test_that("first function works", {
+  a <- testthat::capture_output(first_function())
+  testthat::expect_equal(a, "Hello World!")
+})
